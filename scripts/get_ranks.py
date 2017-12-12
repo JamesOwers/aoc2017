dab772886ec9d3150f3f0d5c0d105afd89aafe6a
kungfujam <james.f.owers+github@gmail.com>
2017-12-12 23:16:23 +0000

    Added download function

--- scripts/get_ranks.py
+++ scripts/get_ranks.py
@@ -8,15 +8,29 @@ Created on Tue Dec 12 05:46:57 2017
 import json
 import pandas as pd
 from collections import defaultdict
+import requests
+import sys
+from tabulate import tabulate
 
 
+SESSION = ''  # get from the browser
 PROJECT_HOME = '/Users/kungfujam/git/aoc2017'
+LEADER_URL = 'https://adventofcode.com/2017/leaderboard/private/view/222021.json'
 
 
 def load_leaderboard():
-    with open('{}/leaderboard.json'.format(PROJECT_HOME)) as f:
-        f_str = f.read().replace('\r\n', '').replace('\n', '')
-        leaderboard = json.loads(f_str)
+    try:
+        with open('{}/leaderboard.json'.format(PROJECT_HOME)) as f:
+            f_str = f.read().replace('\r\n', '').replace('\n', '')
+            leaderboard = json.loads(f_str)
+        print('Read local json...\n\n')
+    except:
+        resp = requests.get('{}'.format(LEADER_URL),
+                            cookies = {'session': SESSION})
+        leaderboard = json.loads(resp.text)
+        with open('{}/leaderboard.json'.format(PROJECT_HOME), 'w') as f:
+            json.dump(leaderboard, f)
+        print('Downloaded json from website...\n\n')
     return leaderboard
 
 
@@ -53,15 +67,18 @@ def load_days(days=None):
         days = [days]
     day_times = get_day_times(leaderboard, days)
     for day in days:
-        pass   
+        pass 
     return day_times    
     
 
 def main(days=None):
     day_times = load_days(days)
     df = day_times_df(day_times)
+#    print(tabulate(df, headers='keys', tablefmt='psql'))
+    print(df)
     return df   
     
     
 if __name__ == '__main__':
-    main()
+    args = sys.argv
+    df = main(days=args)
