cf33458b0965df9a3a73fed9df0aa62657e8683d
kungfujam <james.f.owers+github@gmail.com>
2017-12-14 00:58:42 +0000

    Day 13 done :weary:
    There's obviously a lovely maths one liner for this and I couldn't
    be bothered to find it!

--- day_13.py
+++ day_13.py
@@ -3,7 +3,12 @@ import os
 from my_utils.tests import test_function
 
 
-def part_1():
+def list_to_dict(str_list):
+    dict_list = [ss.split(': ') for ss in str_list]
+    return {int(ii):int(jj) for ii, jj in dict_list}
+
+
+def part_1(str_list):
     """Function which calculates the solution to part 1
     
     Arguments
@@ -12,10 +17,19 @@ def part_1():
     Returns
     -------
     """
-    return None
+    wall_dict = list_to_dict(str_list)
+    trip_severity = 0
+    for depth in wall_dict:
+        rng = wall_dict[depth]
+        if depth % ((rng - 2)*2 + 2) == 0:
+            severity = depth * rng
+            trip_severity += severity   
+            print('Caught @ {}. Cost {}. Tot {}'.\
+                  format(depth, severity, trip_severity))
+    return trip_severity
 
 
-def part_2():
+def part_2(str_list, max_iters=10000000):
     """Function which calculates the solution to part 2
     
     Arguments
@@ -24,7 +38,15 @@ def part_2():
     Returns
     -------
     """
-    return None
+    wall_dict = list_to_dict(str_list)
+    for ii in range(max_iters):
+        caught = 0
+        for depth in wall_dict:
+            rng = wall_dict[depth]
+            if (depth + ii) % ((rng - 2)*2 + 2) == 0:
+                caught = 1
+        if caught == 0:
+            return ii
 
 
 def main(test_datas, functions, puzzle_input=None, test_functions=None):
@@ -46,26 +68,31 @@ if __name__ == "__main__":
     # Testing data: 
     #    - each element of input list will be passed to function
     #    - the relative element in output list is the expected output
+    test1_str = """0: 3
+1: 2
+4: 4
+6: 4"""
+    test1_str = test1_str.split('\n')
     test_data1 = {
-        'inputs': [],
-        'outputs': []
+        'inputs': [test1_str],
+        'outputs': [24]
     }
     test_data2 = {
-        'inputs': [],
-        'outputs': []
+        'inputs': [test1_str],
+        'outputs': [10]
     }
     
     # Code to import the actual puzzle input
-    with open('./inputs/day_x.txt') as f:
-        puzzle_input = f.read().strip()
-#        puzzle_input = [line.rstrip('\n') for line in f]
+    with open('./inputs/day_13.txt') as f:
+#        puzzle_input = f.read().strip()
+        puzzle_input = [line.rstrip('\n') for line in f]
 
     # Main call: performs testing and calculates puzzle outputs
-    main(test_datas=[test_data1],
-         functions=[part_1],
-         puzzle_input=None,
-         test_functions=None)
+#    main(test_datas=[test_data1],
+#         functions=[part_1],
+#         puzzle_input=puzzle_input,
+#         test_functions=None)
 
-    # main(test_datas=[test_data1, test_data2],
-    #      functions=[part_1, part_2],
-    #      puzzle_input=puzzle_input)
+    main(test_datas=[test_data1, test_data2],
+         functions=[part_1, part_2],
+         puzzle_input=puzzle_input)
