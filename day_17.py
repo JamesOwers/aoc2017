dec1441ec4b093d2c75dc543f69241d0a0f81fae
kungfujam <james.f.owers+github@gmail.com>
2017-12-17 14:51:37 +0000

    Finish day 17

--- day_17.py
+++ day_17.py
@@ -3,7 +3,31 @@ import os
 from my_utils.tests import test_function
 
 
-def part_1():
+def create_buff(step_size, buff_max=2017):
+    buff = [0]
+    curr_pos = 0
+    for buff_len in range(1, buff_max+1):
+        curr_pos = (curr_pos + step_size) % buff_len
+        curr_pos += 1
+        buff.insert(curr_pos, buff_len)
+    return buff
+
+
+def simulate_buff(step_size, buff_max=2017):
+    zero_pos = 0
+    curr_pos = 0
+    val_after_zero = None
+    for buff_len in range(1, buff_max+1):
+        curr_pos = (curr_pos + step_size) % buff_len
+        curr_pos += 1
+        if curr_pos == zero_pos + 1:
+            val_after_zero = buff_len
+        if curr_pos <= zero_pos:  # i.e. insert before if index same
+            zero_pos += 1
+    return val_after_zero
+
+
+def part_1(step_size, buff_max=2017):
     """Function which calculates the solution to part 1
     
     Arguments
@@ -12,10 +36,12 @@ def part_1():
     Returns
     -------
     """
-    return None
+    buff = create_buff(step_size, buff_max=buff_max)
+    idx = buff.index(buff_max)
+    return buff[idx + 1 % len(buff)]
 
 
-def part_2():
+def part_2(step_size):
     """Function which calculates the solution to part 2
     
     Arguments
@@ -24,7 +50,7 @@ def part_2():
     Returns
     -------
     """
-    return None
+    return simulate_buff(step_size, int(50*1e6))
 
 
 def main(test_datas, functions, puzzle_input=None, test_functions=None):
@@ -47,8 +73,8 @@ if __name__ == "__main__":
     #    - each element of input list will be passed to function
     #    - the relative element in output list is the expected output
     test_data1 = {
-        'inputs': [],
-        'outputs': []
+        'inputs': [3],
+        'outputs': [638]
     }
     test_data2 = {
         'inputs': [],
@@ -56,14 +82,15 @@ if __name__ == "__main__":
     }
     
     # Code to import the actual puzzle input
-    with open('./inputs/day_x.txt') as f:
+    with open('./inputs/day_17.txt') as f:
         puzzle_input = f.read().strip()
+        puzzle_input = int(puzzle_input)
 #        puzzle_input = [line.rstrip('\n') for line in f]
 
     # Main call: performs testing and calculates puzzle outputs
-    main(test_datas=[test_data1],
-         functions=[part_1],
-         puzzle_input=None,
+    main(test_datas=[],
+         functions=[part_2],
+         puzzle_input=puzzle_input,
          test_functions=None)
 
     # main(test_datas=[test_data1, test_data2],
